8fea9b1db3feb1376b0721491db4e230af9489ae
johnlerma <johnlerma@gmail.com>
2017-03-30 23:29:59 -0700

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,13 +1,27 @@
+function getRandomSpeed() {
+  var min = Math.ceil(200);
+  var max = Math.floor(400);
+  this.speed =  Math.floor(Math.random() * (max - min)) + min;
+}
+
+
+
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x, y) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
-    this.x = 0;
-    this.y = 225;
+    this.x = x;
+    this.y = y;
+    //this.y = 225;
+    var min = Math.ceil(200);
+    var max = Math.floor(400);
+    this.speed = Math.floor(Math.random() * (max - min)) + min;
+    //console.log("speed: " + this.speed);
+        
 };
 
 ////values for  bugs.y
@@ -19,19 +33,45 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-    this.x += this.x + 1 * dt;
+    
+    this.x += this.speed * dt;
+    this.reset();
+    this.checkCollision(player);
+    //console.log("player stuff" + player.x);
 };
 
-//Enemy.prototype.move = function(num) {
-//
-//};
+Enemy.prototype.checkCollision = function(inputPlayer) {
+   
+    //console.log("check inputplayer: " + inputPlayer.x);
+    if (inputPlayer.x < this.x + 75 &&
+        inputPlayer.x + 65 > this.x &&
+        inputPlayer.y < this.y + 50 &&
+        inputPlayer.y + 70 > this.y) {
+        inputPlayer.reset();
+        console.log("collision");
+    }
+  
+};
+
+Enemy.prototype.reset = function(){
+if(this.x >= 490){
+    this.x = -90;
+    //console.log(this.speed);
+    //this.speed = Math.random()*1000;
+var min = Math.ceil(200);
+  var max = Math.floor(400);
+  this.speed =  Math.floor(Math.random() * (max - min)) + min;
+    //console.log("speed " + this.speed);
+
+    }
+};
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
 
     for (var i = 1; i < 4; i++) {
          ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
+}
 };
 
 
@@ -45,13 +85,19 @@ var Player = function() {
 };
 
 Player.prototype.update = function() {
-    //.drawImage(Resources.get(this.sprite), this.x, this.y);
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    //console.log("player y: " + this.y);
 };
 
 
 Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 
+};
+
+Player.prototype.reset = function() {
+    this.y = 410;
+    this.x = 200;
 };
 
 Player.prototype.handleInput = function(event){
@@ -69,13 +115,13 @@ Player.prototype.handleInput = function(event){
            }
             break;
         case "up":
-            if (this.y >= 140){
-            this.y -= 84;
+            if (this.y >= 45){
+            this.y -= 85;
            }
             break;
         case "down":
            if (this.y <= 409){
-            this.y += 84;
+            this.y += 85;
            }
             break;
         default:
@@ -104,9 +150,51 @@ document.addEventListener('keyup', function(e) {
 
     player.handleInput(allowedKeys[e.keyCode]);
 });
-for (var i = 1; i < 4; i++) {
-    allEnemies.push( new Enemy());
-    console.log("hi");
-    console.log(allEnemies);
+//for (var i = 1; i < 4; i++) {
+//    allEnemies.push( new Enemy());
+//    console.log("hi");
+//    console.log(allEnemies);
+//};
+
+var buggy1 = new Enemy(-130, 225);
+var buggy2 = new Enemy(-130, 142);
+var buggy3 = new Enemy(-130, 60);
+allEnemies.push(buggy1);
+allEnemies.push(buggy2);
+allEnemies.push(buggy3);
+//console.log(buggy1);
+
+
+var Gem = function() {
+    this.sprite = 'images/gem-orange-small.png';
+    this.x = 315;
+    this.y = 48;
+    this.width = 10;
+    this.height = 10;
 };
 
+Gem.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Gem.prototype.update = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    this.checkCollision(player);
+   // console.log("xxx");
+};
+
+Gem.prototype.checkCollision = function(inputPlayer) {
+    //console.log("gem start");
+    if (inputPlayer.x < this.x + 75 &&
+        inputPlayer.x + 65 > this.x &&
+        inputPlayer.y < this.y + 50 &&
+        inputPlayer.y + 70 > this.y) {
+        //inputPlayer.reset();
+        
+        this.x = -100;
+        console.log("gem collision");
+    }
+  
+};
+
+var gem = new Gem();
